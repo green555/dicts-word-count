79a3eac785f23dd0e13d5d7df3f77e83e6246c8a
Wei Wei <weiweibcm@gmail.com>
2022-02-08 20:11:54 -0800

    wordcount.py completed

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,13 @@
 """Count words in file."""
+def count_words(filename):
+    text = open(filename)
+    word_count = {}
+    for line in text:
+        words = line.rstrip().split(" ")
+        for word in words:
+            word_count[word] = word_count.get(word, 0) + 1
+    for key, value in word_count.items():
+        print("{} {}".format(key, value))
 
+count_words("test.txt")
 
-# put your code here.
