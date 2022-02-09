2a974cc46409835fe5d71768a80a6b6541793df4
Wei Wei <weiweibcm@gmail.com>
2022-02-08 21:01:39 -0800

    sorted list

--- wordcount.py
+++ wordcount.py
@@ -1,18 +1,28 @@
 import sys
+from collections import Counter
 
 """Count words in file."""
 def count_words(filename):
     text = open(filename)
-    word_count = {}
-    for line in text:
-        words = line.split(" ")
+    text = text.read()
+    text = text.replace("\n", " ").strip()
+    words = text.split(" ")
+    words = [word.strip(",.\n.?").lower() for word in words]
+
+    word_count = Counter(words)
+
+    #result = sorted(word_count.items())
+    #print (result)
+
+    #for line in text:
+        #words = line.split(" ")
         #print(type(words))
         #words = [word.strip(".,?") for word in words]
-        for word in words:
+        #for word in words:
             #print(type(word))
-            word = word.strip(",.\n.?").lower()
-            word_count[word] = word_count.get(word, 0) + 1
-    for key, value in word_count.items():
+            #word = word.strip(",.\n.?").lower()
+            #word_count[word] = word_count.get(word, 0) + 1
+    for key, value in sorted(word_count.items()):
         print("{} {}".format(key, value))
 
 file_name = sys.argv[1]
