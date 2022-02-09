f81b7fd0ce3ec3bf9ff5b83403f39103ea97ec70
Wei Wei <weiweibcm@gmail.com>
2022-02-08 20:32:31 -0800

    further study completed

--- wordcount.py
+++ wordcount.py
@@ -1,13 +1,20 @@
+import sys
+
 """Count words in file."""
 def count_words(filename):
     text = open(filename)
     word_count = {}
     for line in text:
-        words = line.rstrip().split(" ")
+        words = line.split(" ")
+        #print(type(words))
+        #words = [word.strip(".,?") for word in words]
         for word in words:
+            #print(type(word))
+            word = word.strip(",.\n.?").lower()
             word_count[word] = word_count.get(word, 0) + 1
     for key, value in word_count.items():
         print("{} {}".format(key, value))
 
-count_words("test.txt")
+file_name = sys.argv[1]
+count_words(file_name)
 
